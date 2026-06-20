7be538287a3e589957d34860474e9ef3ad1fa6e0
Nitesh150791 <109646228+Nitesh150791@users.noreply.github.com>
2026-06-21 00:28:45 +0530

    Fix comment formatting in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#This is code from https://github.com/LondheShubham153/flask-app-ecs.git repo
+#This is code from https://github.com/LondheShubham153/flask-app-ecs.git
 from flask import Flask, render_template
 app = Flask(__name__)
 
